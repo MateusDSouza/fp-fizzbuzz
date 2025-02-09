c4e8e5a926ba9ce7e87ddb67225eec8ec904e4bd
Mateus D Souza <desouza.mateus1996@gmail.com>
2025-02-09 14:05:24 +0100

    Cleaning the scala docs

--- src/main/scala/AppFizzBuzz.scala
+++ src/main/scala/AppFizzBuzz.scala
@@ -1,37 +1,17 @@
-object AppFizzBuzz extends App {
+object AppFizzBuzz {
 
-  /** Generates a list of numbers from 1 to N.
-    *
-    * @param N The upper limit of the range (inclusive).
-    * @return A list containing numbers from 1 to N.
-    */
   val mockStreamNumbers: Long => List[Long] = (N: Long) => {
     (1 to N.toInt).toList.map(_.toLong) // Convert to Long after range creation
   }
 
-  /** Checks if a number is divisible by 3.
-    *
-    * @param n The number to check.
-    * @return `Some("Fizz")` if the number is divisible by 3, otherwise `None`.
-    */
   val isFizz: Long => Option[String] = (n: Long) => {
     if (n % 3 == 0) Some(s"${n} - Fizz") else None
   }
 
-  /** Checks if a number is divisible by 5.
-    *
-    * @param n The number to check.
-    * @return `Some("Buzz")` if the number is divisible by 5, otherwise `None`.
-    */
   val isBuzz: Long => Option[String] = (n: Long) => {
     if (n % 5 == 0) Some(s"${n} - Buzz") else None
   }
 
-  /** Checks if a number is divisible by both 3 and 5.
-    *
-    * @param n The number to check.
-    * @return `Some("FizzBuzz")` if the number is divisible by both 3 and 5, otherwise `None`.
-    */
   val isFizzBuzz: Long => Option[String] = (n: Long) => {
     if (n % 3 == 0 && n % 5 == 0) Some(s"${n} - FizzBuzz") else None
   }
